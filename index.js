0205e29aed584a049f4a183e4c0fee067df78a2d
sanket <sanketbhanuse9@gmail.com>
2024-07-01 06:03:29 +0530

    create signup api

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+require('./DB/config');
+const product = require('./DB/users');
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.get('',(req,res)=>{
-   res.send("backend run");
+app.post('/signup',async (req,res)=>{
+   const data = new product(req.body);
+   const result = await data.save();
+   res.send(result);
 })
 app.listen(5000);
