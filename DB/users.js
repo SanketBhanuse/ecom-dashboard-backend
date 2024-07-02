57d1c1dadf94db38ad8be8c075e4310cec77df5a
sanket bhanuse <sanketbhanuse9@gmail.com>
2024-07-02 13:40:53 -0700

    done changes in login and signup

--- DB/users.js
+++ DB/users.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-const productSchema = mongoose.Schema({
+const UserSchema = mongoose.Schema({
     name:String,
     email:String,
     password:String
 })
 
-module.exports = mongoose.model("users",productSchema);
+module.exports = mongoose.model("users",UserSchema);
--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const cors = require('cors');
 require('./DB/config');
-const product = require('./DB/users');
+const user = require('./DB/users');
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.post('/signup',async (req,res)=>{
-   const data = new product(req.body);
-   const result = await data.save();
+   const data = new user(req.body);
+   let result = await data.save();
+   result = result.toObject();
+   delete result.password
    res.send(result);
+})
+
+app.post('/login', async(req,res)=>{
+   const result = await user.findOne(req.body).select("-password");
+   if(req.body.email && req.body.password){
+      if(result){
+         res.send(result);
+      }else{
+         res.send({result:"user not found"});
+      }
+   }else{
+      res.send({result:"user not found"});
+   }
 })
 app.listen(5000);
